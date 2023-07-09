b9f40f39c4905d79caff276ae1d3a38493cae939
Dylan Rae <dylan@covalenthq.com>
2023-07-08 19:49:20 -0700

    added wallet and transaction summaries

--- pages/api/image-gen.tsx
+++ pages/api/image-gen.tsx
@@ -5,26 +5,60 @@ const cov_key = process.env.COVALENT_KEY;
 const cov_req_headers = new Headers();
 cov_req_headers.set('Authorization', 'Bearer ' + cov_key);
 
-type Data = {
-  ChainDataResponse: CovalentResponseData;
-  ChainStatusResponse: CovalentResponseData;
+class UserConfig {
+  chain: string;
+  address: string;
+  currency: string;
+  
+  constructor(req: NextApiRequest) {
+    this.chain = req.query.chain as string ? req.query.chain as string : 'eth-mainnet';
+    this.address = req.query.address as string ? req.query.address as string : 'demo.eth';
+    this.currency = req.query.currency as string ? req.query.currency as string : 'USD';
+  }
 }
 
-type ChainData = {
+type Balance = {
+  contract_decimals: number;
+  contract_name: string;
+  contract_ticker_symbol: string;
+  contract_address: string;
+  logo_url: string;
+  type: string;
+  balance: number;
+  quote_rate: number;
+  quote: number;
+};
+
+type TransactionSummary = {
+  count: number;
+  earliest: {
+    block_signed_at: Date;
+    tx_hash: string;
+    tx_detail_link: string;
+  }
+  latest: {
+    block_signed_at: Date;
+    tx_hash: string;
+    tx_detail_link: string;
+  };
+}
+
+type ChainInfo = {
   name: string;
   logo_url: string;
 };
 
-type ChainStatusData = {
+type ChainStatus = {
   name: string;
   synced_block_height: number;
 };
 
 type CovalentResponseData = {
-  chain: ChainData[];
-  chain_status: ChainStatusData[];
+  chainInfos: ChainInfo[];
+  chainStatuses: ChainStatus[];
+  balances: Balance[];
+  transactionSummary: TransactionSummary[];
 };
-
 
 type RawCovalentResponse = {
   data: {
@@ -37,7 +71,7 @@ type RawCovalentResponse = {
 };
 
 
-async function fetchChainData(): Promise<ChainData[]> {
+async function fetchChainInfo(): Promise<ChainInfo[]> {
   const response = await fetch('https://api.covalenthq.com/v1/chains/', {
     method: 'GET',
     headers: cov_req_headers,
@@ -48,11 +82,11 @@ async function fetchChainData(): Promise<ChainData[]> {
   }
 
   const rawResult: RawCovalentResponse = await response.json();
-  const res: ChainData[] = rawResult.data.items;
+  const res: ChainInfo[] = rawResult.data.items;
   return res;
 }
 
-async function fetchChainStatusList(): Promise<ChainStatusData[]> {
+async function fetchChainStatus(): Promise<ChainStatus[]> {
   const response = await fetch('https://api.covalenthq.com/v1/chains/status/', {
     method: 'GET',
     headers: cov_req_headers,
@@ -63,20 +97,53 @@ async function fetchChainStatusList(): Promise<ChainStatusData[]> {
   }
 
   const rawResult: RawCovalentResponse = await response.json();
-  const res: ChainStatusData[] = rawResult.data.items;
+  const res: ChainStatus[] = rawResult.data.items;
   return res;
 }
 
-async function fetchCovalentData(): Promise<CovalentResponseData> {
-    const [chains, chainStatuses] = await Promise.all([
-      fetchChainData(),
-      fetchChainStatusList()
+async function fetchBalances(userConfig: UserConfig): Promise<Balance[]> {
+  const response = await fetch('https://api.covalenthq.com/v1/' + userConfig.chain + '/address/' + userConfig.address + '/balances_v2/?quote-currency=' + userConfig.currency, {
+    method: 'GET',
+    headers: cov_req_headers,
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch Covalent data');
+  }
+
+  const rawResult: RawCovalentResponse = await response.json();
+  const res: Balance[] = rawResult.data.items;
+  return res;
+}
+
+async function fetchTransactionSummary(userConfig: UserConfig): Promise<TransactionSummary[]> {
+  const response = await fetch('https://api.covalenthq.com/v1/' + userConfig.chain + '/address/' + userConfig.address + '/transactions_summary/', {
+    method: 'GET',
+    headers: cov_req_headers,
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch Covalent data');
+  }
+
+  const rawResult: RawCovalentResponse = await response.json();
+  const res: TransactionSummary[] = rawResult.data.items;
+  return res;
+}
+
+async function fetchCovalentData(userConfig: UserConfig): Promise<CovalentResponseData> {
+    const [chains, chainStatuses, balances, transactionSummary] = await Promise.all([
+      fetchChainInfo(),
+      fetchChainStatus(),
+      fetchBalances(userConfig),
+      fetchTransactionSummary(userConfig)
     ]);
 
-
     const res:CovalentResponseData = {
-        chain: chains,
-        chain_status: chainStatuses
+        chainInfos: chains,
+        chainStatuses: chainStatuses,
+        balances: balances,
+        transactionSummary: transactionSummary
     };
 
     return res;
@@ -87,7 +154,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CovalentResponseData>
 ) {
-    const covalentData:CovalentResponseData = await fetchCovalentData();
+    const userConfig: UserConfig = new UserConfig(req);
+    const covalentData:CovalentResponseData = await fetchCovalentData(userConfig);
 
     res.status(200).json(covalentData);
 }
