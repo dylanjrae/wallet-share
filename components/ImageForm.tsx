562360712f84453971c93a24860c72d7432d9593
Dylan Rae <dylan@covalenthq.com>
2023-07-17 17:21:26 -0700

    naming fixes

--- components/ImageForm.tsx
+++ components/ImageForm.tsx
@@ -4,13 +4,13 @@ import React, { useState } from 'react';
 
 const ImageForm: React.FC = () => {
   const [addressInput, setAddressInput] = useState('demo.eth');
-  const [dropdown1, setDropdown1] = useState('eth-mainnet');
-  const [dropdown2, setDropdown2] = useState('CAD');
-  const [imageUrl, setImageUrl] = useState('https://wallet-share.dylanrae.ca/api/v1/img_generator/?address=demo.eth&chain=eth-mainnet&currency=USD');
+  const [chainInput, setChainInput] = useState('eth-mainnet');
+  const [currencyInput, setCurrencyInput] = useState('USD');
+  const [imageUrl, setImageUrl] = useState(`https://wallet-share.dylanrae.ca/api/v1/img_generator/?address=${addressInput}&chain=${chainInput}&currency=${currencyInput}`);
 
   const handleGenerateClick = () => {
     const baseUrl = 'https://wallet-share.dylanrae.ca/api/v1/img_generator/';
-    const queryParams = `?address=${addressInput}&chain=${dropdown1}&currency=${dropdown2}`;
+    const queryParams = `?address=${addressInput}&chain=${chainInput}&currency=${currencyInput}`;
     const generatedUrl = baseUrl + queryParams;
     setImageUrl(generatedUrl);
   };
@@ -23,21 +23,21 @@ const ImageForm: React.FC = () => {
         onChange={(e) => setAddressInput(e.target.value)}
       />
       <select
-        value={dropdown1}
-        onChange={(e) => setDropdown1(e.target.value)}
+        value={chainInput}
+        onChange={(e) => setChainInput(e.target.value)}
       >
         <option value="eth-mainnet">Ethereum Mainnet</option>
         <option value="matic-mainnet">Polygon Mainnet</option>
         <option value="moonbeam-mainnet">Moonbeam Mainnet</option>
       </select>
       <select
-        value={dropdown2}
-        onChange={(e) => setDropdown2(e.target.value)}
+        value={currencyInput}
+        onChange={(e) => setCurrencyInput(e.target.value)}
       >
         <option value="USD">USD</option>
         <option value="CAD">CAD</option>
         <option value="EUR">EUR</option>
-        <option value="IDR">INR</option>
+        <option value="INR">INR</option>
       </select>
       <button onClick={handleGenerateClick}>Generate</button>
       <p>{imageUrl}</p>
