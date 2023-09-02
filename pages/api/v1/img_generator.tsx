5046245d7d020316c8a2a5a6316e650b90c5e93e
Dylan Rae <55325297+dylanjrae@users.noreply.github.com>
2023-09-01 22:14:14 -0600

    migrate to covalent sdk

--- pages/api/v1/img_generator.tsx
+++ pages/api/v1/img_generator.tsx
@@ -1,21 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Client } from '@covalenthq/client-sdk';
+import { Chains, Quotes } from '@covalenthq/client-sdk/dist/services/Client';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import build from 'next/dist/build';
-// import { Client } from '@covalenthq/client-sdk';
 
-const cov_key = process.env.COVALENT_KEY;
+
+if (!process.env.COVALENT_KEY) {
+  throw new Error('Missing internally required environment variable.');
+}
+const cov_key: string = process.env.COVALENT_KEY;
+const covaClient: Client = new Client(cov_key);
+
 const cov_req_headers = new Headers();
 cov_req_headers.set('Authorization', 'Bearer ' + cov_key);
 
 class UserConfig {
-  chain: string;
+  chain: Chains;
   address: string;
-  currency: string;
+  currency: Quotes;
   
   constructor(req: NextApiRequest) {
-    this.chain = req.query.chain as string ? req.query.chain as string : 'eth-mainnet';
+    this.chain = req.query.chain as Chains ? req.query.chain as Chains : 'eth-mainnet';
     this.address = req.query.address as string ? req.query.address as string : 'demo.eth';
-    this.currency = req.query.currency as string ? req.query.currency as string : 'USD';
+    this.currency = req.query.currency as Quotes ? req.query.currency as Quotes : 'USD';
   }
 }
 
@@ -26,7 +33,7 @@ type Balance = {
   contract_address: string;
   logo_url: string;
   type: string;
-  balance: number;
+  balance: bigint | null;
   quote_rate: number;
   quote: number;
 };
@@ -34,12 +41,12 @@ type Balance = {
 type TransactionSummary = {
   total_count: number;
   earliest_transaction: {
-    block_signed_at: string;
+    block_signed_at: Date;
     tx_hash: string;
     tx_detail_link: string;
   }
   latest_transaction: {
-    block_signed_at: string;
+    block_signed_at: Date;
     tx_hash: string;
     tx_detail_link: string;
   };
@@ -55,11 +62,6 @@ type ChainActivityEvent = ChainInfo & {
   last_seen_at: Date;
 };
 
-type ChainStatus = {
-  name: string;
-  synced_block_height: number;
-};
-
 type CovalentBatchResponseData = {
   address: string;
   chainInfos: ChainInfo[];
@@ -68,109 +70,20 @@ type CovalentBatchResponseData = {
   transactionSummary: TransactionSummary[];
 };
 
-type RawCovalentResponse = {
-  data: CovalentResponseData;
-  error: boolean;
-  error_message: null | string;
-  error_code: null | string;
-};
-
-type CovalentResponseData = {
-  updated_at: string;
-  address: string;
-  items: any[];
-}
-
-
-async function fetchChainInfo(): Promise<CovalentResponseData> {
-  const response = await fetch('https://api.covalenthq.com/v1/chains/', {
-    method: 'GET',
-    headers: cov_req_headers,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch Covalent data: ' + response.statusText);
-  }
-
-  const rawResult: RawCovalentResponse = await response.json();
-  const res: CovalentResponseData = rawResult.data;
-  return res;
-}
-
-async function fetchChainStatus(): Promise<CovalentResponseData> {
-  const response = await fetch('https://api.covalenthq.com/v1/chains/status/', {
-    method: 'GET',
-    headers: cov_req_headers,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch Covalent data: ' + response.statusText);
-  }
-
-  const rawResult: RawCovalentResponse = await response.json();
-  const res: CovalentResponseData = rawResult.data;
-  return res;
-}
-
-async function fetchBalances(userConfig: UserConfig): Promise<CovalentResponseData> {
-  const response = await fetch('https://api.covalenthq.com/v1/' + userConfig.chain + '/address/' + userConfig.address + '/balances_v2/?quote-currency=' + userConfig.currency, {
-    method: 'GET',
-    headers: cov_req_headers,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch Covalent data: ' + response.statusText);
-  }
-
-  const rawResult: RawCovalentResponse = await response.json();
-  const res: CovalentResponseData = rawResult.data;
-  return res;
-}
-
-async function fetchTransactionSummary(userConfig: UserConfig): Promise<CovalentResponseData> {
-  const response = await fetch('https://api.covalenthq.com/v1/' + userConfig.chain + '/address/' + userConfig.address + '/transactions_summary/', {
-    method: 'GET',
-    headers: cov_req_headers,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch Covalent data: ' + response.statusText);
-  }
-
-  const rawResult: RawCovalentResponse = await response.json();
-  const res: CovalentResponseData = rawResult.data;
-  return res;
-}
-
-async function fetchChainActivity(userConfig: UserConfig): Promise<CovalentResponseData> {
-  const response = await fetch('https://api.covalenthq.com/v1/labs/activity/' + userConfig.address + '/', {
-    method: 'GET',
-    headers: cov_req_headers,
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch Covalent data: ' + response.statusText);
-  }
-
-  const rawResult: RawCovalentResponse = await response.json();
-  const res: CovalentResponseData = rawResult.data;
-  return res;
-}
-
-async function fetchCovalentData(userConfig: UserConfig): Promise<CovalentBatchResponseData> {
-    const [chainsResp, chainActivityResp, balancesResp, transactionSummaryResp] = await Promise.all([
-      fetchChainInfo(),
-      fetchChainActivity(userConfig),
-      fetchBalances(userConfig),
-      fetchTransactionSummary(userConfig),
+async function fetchCovalentData(userConfig: UserConfig, covaClient: Client): Promise<CovalentBatchResponseData> {
+    const [chainsResp, addressActivityResp, balancesResp, transactionSummaryResp] = await Promise.all([
+      covaClient.BaseService.getAllChains(),
+      covaClient.BaseService.getAddressActivity(userConfig.address),
+      covaClient.BalanceService.getTokenBalancesForWalletAddress(userConfig.chain, userConfig.address, { quoteCurrency: userConfig.currency }),
+      covaClient.TransactionService.getTransactionSummary(userConfig.chain, userConfig.address),
     ]);
 
     const res:CovalentBatchResponseData = {
-        chainInfos: chainsResp.items,
-        chainActivity: chainActivityResp.items,
-        balances: balancesResp.items,
-        address: balancesResp.address,
-        transactionSummary: transactionSummaryResp.items,
+        chainInfos: chainsResp.data.items,
+        chainActivity: addressActivityResp.data.items,
+        balances: balancesResp.data.items,
+        address: balancesResp.data.address,
+        transactionSummary: transactionSummaryResp.data.items,
     };
 
     return res;
@@ -182,7 +95,6 @@ async function fetchBase64Image(url: string): Promise<string> {
   const base64Image = btoa(await imageBlob.text());
 
   return `data:image/svg+xml;base64,${base64Image}`;
-
 }
 
 function findChainByChainName(chainName: string, chainInfos: ChainInfo[]): ChainInfo {
@@ -197,8 +109,6 @@ function calculateWalletNetWorth(balances: Balance[]): number {
   });
   return netWorth;
 }
-
-// type Quotes = "USD" | "CAD";
 
 function formatAsCurrency(amount: number, currencyCode: string ): string {
   const formattedNumber = new Intl.NumberFormat('en-US', {
@@ -289,7 +199,7 @@ export default async function handler(
   res: NextApiResponse<string>
 ) {
     const userConfig: UserConfig = new UserConfig(req);
-    const covalentData: CovalentBatchResponseData = await fetchCovalentData(userConfig);
+    const covalentData: CovalentBatchResponseData = await fetchCovalentData(userConfig, covaClient);
     
     const svg: string = await buildSVG(userConfig, covalentData);
 
