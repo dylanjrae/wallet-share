9e18f7832483fb6f0e83008d3f4e9bfa9df2b3dc
Dylan Rae <55325297+dylanjrae@users.noreply.github.com>
2023-11-18 16:14:43 +0300

    add docs link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,18 @@ export default async function Home() {
 
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-between">
+      <main className="flex min-h-screen flex-col items-center justify-between relative">
         <div>
           <h1 className="text-center text-4xl tracking-wide font-mono mt-12">Wallet Card Generator</h1>
           <ImageForm rawChains={JSON.stringify(chains)}/>
         </div>
+        <h3 className="absolute bottom-0 mb-8 text-center text-sm font-mono">
+          Please visit the docs{' '}
+          <a href="https://github.com/dylanjrae/wallet-share" className="underline text-blue-300" target="_blank" rel="noopener noreferrer">
+            here
+          </a>
+          {' '} for more details 🙂
+        </h3>
       </main>
     </div>
   )
